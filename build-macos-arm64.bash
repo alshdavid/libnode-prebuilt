2dd1c0f7b7ae4cb49487e2e445a8cf2db860a65c
David Alsh <alshdavid@gmail.com>
2025-05-03 21:18:26 +1000

    initial commit

---
+++ build-macos-arm64.bash
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+set -e 
+
+os="macos"
+arch="arm64"
+
+function prepare() {
+  sudo chown -R $(whoami) $(brew --prefix)/*
+
+  # homebrew fails to update python 3.9.1 to 3.9.1.1 due to unlinking failure
+  sudo rm -f /usr/local/bin/2to3 || true
+  # homebrew fails to update python from 3.9 to 3.10 due to another unlinking failure
+  sudo rm -f /usr/local/bin/idle3 || true
+  sudo rm -f /usr/local/bin/pydoc3 || true
+  sudo rm -f /usr/local/bin/python3 || true
+  sudo rm -f /usr/local/bin/python3-config || true
+
+  brew install git node ninja nasm ccache
+}
+
+function clone() {
+  if [ "$NODEJS_GIT" = "" ]; then
+    echo "Missing \$NODEJS_GIT"
+    exit 1
+  fi
+
+  if [ "$NODEJS_BRANCH" = "" ]; then
+    echo "Missing \$NODEJS_BRANCH"
+    exit 1
+  fi
+
+  git clone "$NODEJS_GIT" --branch "$NODEJS_BRANCH" --depth=1 ./node
+}
+
+function build() {
+  cd ./node  
+  ./configure \
+    --shared \
+    --dest-cpu arm64 \
+    --dest-os mac \
+    --no-cross-compiling \
+    --with-arm-float-abi hard \
+    --with-arm-fpu neon
+  make -j8
+  cd ../
+}
+
+function copy() {
+  rm -rf release/libnode-$os-$arch
+  mkdir -p release/libnode-$os-$arch
+  cp ./node/out/Release/libnode.* ./release/libnode-$os-$arch
+  cp ./node/out/Release/node ./release/libnode-$os-$arch
+  ln ./release/libnode-$os-$arch/libnode.* ./release/libnode-$os-$arch/libnode.dylib
+}
+
+function default() {
+  prepare
+  clone
+  build
+  copy
+}
+
+if [ "$1" = "" ]; then
+  default
+else
+  $1
+fi
