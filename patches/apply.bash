5699b4da8163341836798be676f695938fcca00c
David Alsh <alshdavid@gmail.com>
2025-05-18 13:07:01 +1000

    added patches

--- patches/apply.bash
+++ patches/apply.bash
@@ -4,3 +4,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 cd $SCRIPT_DIR/../node
 git apply ../patches/node_c_embed.patch
+
+if [ ! -f "$SCRIPT_DIR/node_c_embed_unix.patch" ]; then
+    git apply $SCRIPT_DIR/node_c_embed_unux.patch
+fi
--- patches/apply.ps1
+++ patches/apply.ps1
@@ -1,2 +1,6 @@
 cd $PSScriptRoot\..\node
 git apply ..\patches\node_c_embed.patch
+
+if (Test-Path "$PSScriptRoot\node_c_embed_windows.patch") {
+    git apply ..\patches\node_c_embed_windows.patch
+}
