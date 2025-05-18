9bfa7e27d579c058cd7a5f04d108740a5f72dde7
David Alsh <alshdavid@gmail.com>
2025-05-18 13:26:16 +1000

    patches

--- patches/apply.bash
+++ patches/apply.bash
@@ -3,8 +3,16 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 cd $SCRIPT_DIR/../node
-git apply ../patches/node_c_embed.patch
 
-if [ ! -f "$SCRIPT_DIR/node_c_embed_unix.patch" ]; then
-    git apply $SCRIPT_DIR/node_c_embed_unux.patch
-fi
+for name in "$SCRIPT_DIR/all/*.patch"; do  
+    echo Applying $name
+    git apply $name
+done
+
+for name in "$SCRIPT_DIR/$1/*.patch"; do  
+    if [ ! -f $name ]; then
+        break
+    fi
+    echo Applying $name
+    git apply $name
+done
